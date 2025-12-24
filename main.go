9e780a528d7d4d08f62abadfba6007c0d5cbe320
wbhemingway <william.b.hemingway@gmail.com>
2025-12-24 12:10:57 -0500

    serves basic html and image

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import "net/http"
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+	
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 	
 	serve := http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: mux,
 	}
+	
 	serve.ListenAndServe()
 }
