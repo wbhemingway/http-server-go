f2b7d469e870b37f92ae68e093c6bfd058420d75
wbhemingway <william.b.hemingway@gmail.com>
2026-01-05 12:39:44 -0500

    added validation endpoint

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync/atomic"
 )
+
+const metricsTemplate string = `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`
 
 func main() {
 	const filepathRoot = "."
@@ -18,14 +26,18 @@ func main() {
 			apiCfg.middlewareMetricsInc(http.FileServer(http.Dir("."))),
 		),
 	)
-	mux.HandleFunc("GET /healthz", healthHandler)
-	mux.HandleFunc("GET /metrics", apiCfg.metricsHandler)
-	mux.HandleFunc("POST /reset", apiCfg.resetHandler)
+	mux.HandleFunc("GET /api/healthz", healthHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateHandler)
 	serve := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+
+	fmt.Println("Chirpy Server Started!")
 	serve.ListenAndServe()
+	fmt.Println("Chirpy Server Stopped!")
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
@@ -46,8 +58,8 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	hitStr := fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	hitStr := fmt.Sprintf(metricsTemplate, cfg.fileserverHits.Load())
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte(hitStr))
 }
@@ -56,4 +68,39 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
+}
+
+func validateHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type errMsg struct {
+		Error string `json:"error"`
+	}
+	type sucMsg struct {
+		Valid bool `json:"valid"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	chirp := parameters{}
+	err := decoder.Decode(&chirp)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		dat, _:= json.Marshal(errMsg{Error: "Error decoding posted json"})
+		w.Write(dat)
+		return
+	}
+	
+	if len(chirp.Body) > 140 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		dat, _:= json.Marshal(errMsg{Error: "Chirp is too long"})
+		w.Write(dat)
+		return
+	}
+	
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	dat, _:= json.Marshal(sucMsg{Valid: true})
+	w.Write(dat)
 }
